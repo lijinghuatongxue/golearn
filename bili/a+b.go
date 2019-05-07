eb2c33a8d4ee18a53d93210ccaf9097c8c20e453
lijinghuatongxue <woshilijinghua@gmail.com>
2019-05-07 15:47:51 +0800

    20190507

--- bili/a+b.go
+++ bili/a+b.go
@@ -14,11 +14,17 @@ func main(){
 	for i := 0; i < 100;i++ {
 		go test_print100(i)
 	}
-	// 跑10s，尽管一秒钟就能跑完，但是程序在10s之后才会退出,继续执行下一个任务
-	time.Sleep(10*time.Second)
+	// 跑10s，尽管一秒钟就能跑完，但是程序在2s之后才会退出,继续执行下一个任务
+	//time.Sleep(2*time.Second)
+
+	fmt.Println("a+b example ++++++++++++++++++++++++")
 	// a+b
 	var c int
 	c = add(100,200)
 	go test_goroute(300,300)
 	fmt.Println("a+b =",c)
+	fmt.Println("pipe example ++++++++++++++++++++++++")
+	// 调用test_pipe example
+	test_pipe()
+
 }
---
+++ bili/pipe.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func test_pipe(){
+	pipe := make (chan int,3)
+	pipe <- 1
+	pipe <- 2
+	pipe <- 3
+
+	var t1 int
+	// 变量赋值 需要 =
+	t1 =<- pipe
+	pipe <- 4
+	fmt.Println(t1)
+	// python类似，打印长度
+	fmt.Println(len(pipe))
+
+}
