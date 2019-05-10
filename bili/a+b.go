7801762340deeb1f92fb9ff94e69292d3d3b9446
lijinghuatongxue <woshilijinghua@gmail.com>
2019-05-10 17:21:13 +0800

    go

--- bili/a+b.go
+++ bili/a+b.go
@@ -2,29 +2,20 @@ package main
 
 import (
 	"fmt"
-	_ "time"
+	"time"
+	_"time"
 )
 
-func add(a int, b int) int {
+var pipe chan int
+func add(a int,b int,c chan int){
 	var sum int
-	sum = a + b
-	return sum
+	sum = a+b
+	time.Sleep(1*time.Second)
+	c <- sum
 }
-func main() {
-	for i := 0; i < 100; i++ {
-		go test_print100(i)
-	}
-	// 跑10s，尽管一秒钟就能跑完，但是程序在2s之后才会退出,继续执行下一个任务
-	//time.Sleep(2*time.Second)
-
-	fmt.Println("a+b example ++++++++++++++++++++++++")
-	// a+b
-	var c int
-	c = add(100, 200)
-	go test_goroute(300, 300)
-	fmt.Println("a+b =", c)
-	fmt.Println("pipe example ++++++++++++++++++++++++")
-	// 调用test_pipe example
-	test_pipe()
-
+func main(){
+	pipe = make(chan int,1)
+	go add(2,2,pipe)
+	sum :=<- pipe
+	fmt.Println("sum=",sum)
 }
