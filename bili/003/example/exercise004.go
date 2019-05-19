30625869b818ab86a0d7373da38dffd9346f65e5
unknown <598820383@qq.com>
2019-05-19 22:51:46 +0800

    exercise004

---
+++ bili/003/example/exercise004.go
@@ -0,0 +1,3 @@
+package example
+var Word string = "你好呀"
+var Name string = "李银河"
---
+++ bili/003/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"../example"
+)
+
+func main(){
+	fmt.Println("name=",example.Name)
+	fmt.Println("word=",example.Word)
+
+}
