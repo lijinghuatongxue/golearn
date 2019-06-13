12ca5f920cd4a0d57717689c2f0f149acad34878
lijinghuatongxue <woshilijinghua@gmail.com>
2019-06-13 16:08:25 +0800

    [base]init函数

--- github/init/test.go
+++ github/init/test.go
@@ -1,1 +1,16 @@
-package init
+package main
+
+import (
+	"fmt"
+)
+// init执行优先级比 main 函数高
+var Pi float64 = 45
+
+func init() {
+	//Pi = 4 * math.Atan(1) // init() function computes Pi
+	fmt.Println("Pi")
+}
+func main()  {
+	const Pi  = 1
+	fmt.Println("main 输出：",Pi)
+}
