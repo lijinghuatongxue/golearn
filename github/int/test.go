b5ff574ebac9dd5481b4df72330ab4d41ac162ce
lijinghuatongxue <woshilijinghua@gmail.com>
2019-06-14 16:07:28 +0800

    [note]int and float

--- github/int/test.go
+++ github/int/test.go
@@ -1,1 +1,23 @@
-package int
+package main
+
+import "fmt"
+
+var a float64 = 2.234423423
+var b float64 = 0.2133232222
+
+var c  float32 = 233.2342
+var d  float32 = 234423.23423
+
+var e int64 = 90921312312312312
+var f int64 = 233213123
+
+var g int32 = 89789
+var j int32 = 1121122
+
+func main()  {
+	fmt.Println("float64 a+b=",a+b)
+	fmt.Println("float32 c+d=",c+d)
+	fmt.Println("int64 e+f=",e+f)
+	fmt.Println("int32 g+j=",g+j)
+}
+
