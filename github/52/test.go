19c412835fd78a512770f75b680e401424f07870
lijinghuatongxue <woshilijinghua@gmail.com>
2019-06-25 14:39:44 +0800

    [BASE]多值返回

--- github/52/test.go
+++ github/52/test.go
@@ -1,1 +1,25 @@
-package _2
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var orig string = "222"
+	var orig2 string = "ABC"
+	// var an int
+	// var err error
+
+	fmt.Printf("The size of ints is: %d\n", strconv.IntSize)
+	// 第一个返回值是转换成int的值，第二个返回值判断是否转换成功
+	// 这里的是正确的，能转变为数字
+	_, err := strconv.Atoi(orig)
+	if err != nil{
+		fmt.Println("err！！")
+	}
+	// 这里的是错误的，不能转变为数字，将会打印
+	i, err := strconv.Atoi(orig2)
+	if err != nil{
+		fmt.Println("err！！",i+1)
+		}}
