826736cccd2bb0b0f73c02977694baab503592e5
lijinghuatongxue <woshilijinghua@gmail.com>
2019-06-27 23:31:00 +0800

    [base]for循环的两道测试题

--- github/for/test2.go
+++ github/for/test2.go
@@ -13,7 +13,7 @@ import (
 //GGGGGG
 
 
-// 方法1
+// 方法1 使用 2 层嵌套 for 循环
 func main()  {
 	for i := 1; i <= 25;i++{
 		for g := 1;g < i;g++{
@@ -22,7 +22,7 @@ func main()  {
 		fmt.Println()
 	}
 
-	// 方法2
+	// 方法2 仅用 1 层 for 循环以及字符串连接
 var str string = "G"
 for i := 1;i<25;i++{
 	fmt.Println(str)
