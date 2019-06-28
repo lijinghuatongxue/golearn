70f2d4db73b60635e13d8268f76266a57dee7805
lijinghuatongxue <woshilijinghua@gmail.com>
2019-06-28 09:47:20 +0800

    [base]for循环多姿势

--- github/for/test4.go
+++ github/for/test4.go
@@ -8,4 +8,19 @@ func main()  {
 		i--
 		fmt.Println(i)
 	}
+	//for i := 0; ; i++ {
+	//	fmt.Println("Value of i is now:", i)
+	//}
+	//for i := 0; i < 3; {
+	//	fmt.Println("Value of i:", i)
+	//}
+	s := ""
+	for ; s != "aaaaa"; {
+		fmt.Println("Value of s:", s)
+		s = s + "a"
+	}
+	for i, j, s := 0, 5, "a"; i < 3 && j < 100 && s != "aaaaa"; i, j,
+		s = i+1, j+1, s + "a" {
+		fmt.Println("Value of i, j, s:", i, j, s)
+	}
 }
