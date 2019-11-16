d8bc4371cb77b31922b1ab32bad265c78c66bb26
lijinghuatongxue <598820383@qq.com>
2019-11-16 19:31:14 +0800

    [update]20191116

---
+++ me/zabbix/zabbix.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"github.com/rday/zabbix"
+)
+
+func main() {
+api, err := zabbix.NewAPI("http://zabbix.geekerland.net/api_jsonrpc.php", "Admin", "Swl19960706@.@")
+if err != nil {
+fmt.Println(err)
+return
+}
+
+versionresult, err := api.Version()
+if err != nil {
+fmt.Println(err)
+}
+
+fmt.Println(versionresult)
+
+_, err = api.Login()
+if err != nil {
+fmt.Println(err)
+return
+}
+fmt.Println("Connected to API")
+
+}
